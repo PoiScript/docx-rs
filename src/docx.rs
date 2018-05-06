f6480a6abdee61a030f9bd723805ae2e2a0bfee7
PoiScript <poiscript@gmail.com>
2018-05-06 21:50:04 +0800

    feat: generate docx file

---
+++ src/docx.rs
@@ -0,0 +1,65 @@
+use std::str;
+use std::io::{Write, Seek};
+use app_xml::AppXml;
+use content_types_xml::ContentTypesXml;
+use core_xml::CoreXml;
+use document_xml::DocumentXml;
+use rels::Relationships;
+use zip::result::ZipResult;
+use zip::ZipWriter;
+use zip::write::FileOptions;
+use zip::CompressionMethod;
+
+static APP_XML: &'static str = "docProps/app.xml";
+static CONTENT_TYPES_XML: &'static str = "[Content_Types].xml";
+static CORE_XML: &'static str = "docProps/core.xml";
+static DOCUMENT_XML: &'static str = "word/document.xml";
+
+static RELS: &'static str = "_rels/.rels";
+//static DOCUMENT_RELS: &'static str = "word/_rels/document.xml.rels";
+
+pub struct Docx<'a> {
+  app_xml: AppXml<'a>,
+  content_types_xml: ContentTypesXml<'a>,
+  core_xml: CoreXml<'a>,
+  document_xml: DocumentXml<'a>,
+  rels: Relationships<'a>,
+}
+
+impl<'a> Docx<'a> {
+  pub fn new() -> Docx<'a> {
+    Docx {
+      app_xml: AppXml::default(),
+      content_types_xml: ContentTypesXml::default(),
+      core_xml: CoreXml::default(),
+      document_xml: DocumentXml::new(),
+      rels: Relationships::default(),
+    }
+  }
+
+  pub fn generate<T: Write + Seek>(&self, writer: T) -> ZipResult<()> {
+    let mut zip = ZipWriter::new(writer);
+    let opt = FileOptions::default()
+      .compression_method(CompressionMethod::Deflated)
+      .unix_permissions(0o755);
+
+    zip.start_file(APP_XML, opt)?;
+    zip.write_all(&self.app_xml.generate())?;
+
+    zip.start_file(CONTENT_TYPES_XML, opt)?;
+    zip.write_all(&self.content_types_xml.generate())?;
+
+    zip.start_file(CORE_XML, opt)?;
+    zip.write_all(&self.core_xml.generate())?;
+
+    zip.start_file(DOCUMENT_XML, opt)?;
+    zip.write_all(&self.document_xml.generate())?;
+
+    zip.start_file(RELS, opt)?;
+    zip.write_all(&self.rels.generate())?;
+
+    zip.finish()?;
+
+    Result::Ok(())
+  }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,13 @@
-#[cfg(test)]
-mod tests {
-  #[test]
-  fn it_works() {
-    assert_eq!(2 + 2, 4);
-  }
-}
+extern crate quick_xml;
+extern crate zip;
+
+mod app_xml;
+mod body;
+mod content_types_xml;
+mod core_xml;
+mod document_xml;
+mod docx;
+mod rels;
+mod utility;
+
+pub use docx::Docx;
