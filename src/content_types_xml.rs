8cd8e60b88b98b2745500c52e1220feb3042c9af
PoiScript <poiscript@gmail.com>
2018-05-06 17:31:16 +0800

    feat: generate [Content_Types].xml

---
+++ src/content_types_xml.rs
@@ -0,0 +1,46 @@
+use std::collections::LinkedList;
+
+use utility::LinkUtil;
+
+pub struct ContentTypesXml<'a> {
+  defaults: Vec<(&'a str, &'a str)>,
+  overrides: Vec<(&'a str, &'a str)>,
+}
+
+impl<'a> ContentTypesXml<'a> {
+  pub fn default() -> ContentTypesXml<'a> {
+    ContentTypesXml {
+      defaults: vec![
+        ("rels", "application/vnd.openxmlformats-package.relationships+xml"),
+        ("xml", "application/xml"),
+      ],
+      overrides: vec![
+        ("/word/document.xml", "application/vnd.openxmlformats-officedocument.wordprocessingml.document.main+xml"),
+        ("/word/styles.xml", "application/vnd.openxmlformats-officedocument.wordprocessingml.styles+xml"),
+        ("/docProps/app.xml", "application/vnd.openxmlformats-officedocument.extended-properties+xml"),
+        ("/word/settings.xml", "application/vnd.openxmlformats-officedocument.wordprocessingml.settings+xml"),
+        ("/word/theme/theme1.xml", "application/vnd.openxmlformats-officedocument.theme+xml"),
+        ("/word/fontTable.xml", "application/vnd.openxmlformats-officedocument.wordprocessingml.fontTable+xml"),
+        ("/word/webSettings.xml", "application/vnd.openxmlformats-officedocument.wordprocessingml.webSettings+xml"),
+        ("/docProps/core.xml", "application/vnd.openxmlformats-package.core-properties+xml"),
+      ],
+    }
+  }
+
+  pub fn generate(&self) -> Vec<u8> {
+    let mut events = LinkedList::new();
+
+    for i in 0..self.defaults.len() {
+      events.add_tag_with_attr(b"Default", vec![self.defaults[i]]);
+    }
+
+    for i in 0..self.overrides.len() {
+      events.add_tag_with_attr(b"Override", vec![self.overrides[i]]);
+    }
+
+    events
+      .wrap_tag_with_attr(b"Types", vec![("xmlns", "http://schemas.openxmlformats.org/package/2006/content-types")])
+      .add_decl()
+      .to_xml()
+  }
+}
--- src/utility.rs
+++ src/utility.rs
@@ -9,6 +9,7 @@ use std::collections::LinkedList;
 pub trait LinkUtil<'a> {
   fn add_decl(&mut self) -> &mut Self;
   fn add_tag(&mut self, tag: &'a [u8], content: &'a str) -> &mut Self;
+  fn add_tag_with_attr(&mut self, tag: &'a [u8], attributes: Vec<(&'a str, &'a str)>) -> &mut Self;
   fn warp_tag(&mut self, tag: &'a [u8]) -> &mut Self;
   fn wrap_tag_with_attr(&mut self, tag: &'a [u8], attributes: Vec<(&'a str, &'a str)>) -> &mut Self;
   fn to_xml(&self) -> Vec<u8>;
@@ -24,6 +25,13 @@ impl<'a> LinkUtil<'a> for LinkedList<Event<'a>> {
   fn add_tag(&mut self, tag: &'a [u8], content: &'a str) -> &mut Self {
     self.push_back(Event::Start(BytesStart::borrowed(tag, tag.len())));
     self.push_back(Event::Text(BytesText::from_plain_str(content)));
+    self.push_back(Event::End(BytesEnd::borrowed(tag)));
+
+    self
+  }
+
+  fn add_tag_with_attr(&mut self, tag: &'a [u8], attributes: Vec<(&'a str, &'a str)>) -> &mut Self {
+    self.push_back(Event::Start(BytesStart::borrowed(tag, tag.len()).with_attributes(attributes)));
     self.push_back(Event::End(BytesEnd::borrowed(tag)));
 
     self
