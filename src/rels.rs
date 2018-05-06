64c5c50e125b9a785a7c833d5e33795fecf8c05c
PoiScript <poiscript@gmail.com>
2018-05-06 21:35:24 +0800

    feat: generate id in relationship

--- src/rels.rs
+++ src/rels.rs
@@ -5,28 +5,56 @@ use utility::LinkUtil;
 
 static RELATIONSHIPS_SCHEMAS: &'static str = "http://schemas.openxmlformats.org/package/2006/relationships";
 static OFFICE_DOCUMENT_SCHEMAS: &'static str = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/officeDocument";
+static CORE_SCHEMAS: &'static str = "http://schemas.openxmlformats.org/officedocument/2006/relationships/metadata/core-properties";
+static EXTENDED_SCHEMAS: &'static str = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/extended-properties";
 
-pub enum Relationship<'a> {
-  Document(&'a str),
-  Theme(&'a str),
-  Settings(&'a str),
-  FontTable(&'a str),
-  Styles(&'a str),
-  Image(&'a str),
-  Header(&'a str),
-  Numbering(&'a str),
+// FIXME
+static IDS: [&'static str; 14] = [
+  "rId1",
+  "rId2",
+  "rId3",
+  "rId4",
+  "rId5",
+  "rId6",
+  "rId7",
+  "rId8",
+  "rId9",
+  "rId10",
+  "rId11",
+  "rId12",
+  "rId13",
+  "rId14",
+];
+
+pub enum Relationship {
+  Core,
+  Extended,
+  Document,
+  Theme,
+  Settings,
+  FontTable,
+  Styles,
+  Image,
+  Header,
+  Numbering,
 }
 
 pub struct Relationships<'a> {
-  relationships: Vec<Relationship<'a>>,
+  relationships: Vec<(Relationship, &'a str)>,
 }
 
 impl<'a> Relationships<'a> {
   pub fn default() -> Relationships<'a> {
-    Relationships { relationships: vec![Relationship::Document("word/document.xml")] }
+    Relationships {
+      relationships: vec![
+        (Relationship::Core, "docProps/core.xml"),
+        (Relationship::Extended, "docProps/app.xml"),
+        (Relationship::Document, "word/document.xml"),
+      ]
+    }
   }
 
-  pub fn add_rel(&mut self, rel: Relationship<'a>) {
+  pub fn add_rel(&mut self, rel: (Relationship, &'a str)) {
     self.relationships.push(rel);
   }
 
@@ -34,16 +62,18 @@ impl<'a> Relationships<'a> {
     let mut events = LinkedList::new();
 
     for i in 0..self.relationships.len() {
-      match self.relationships[i] {
-        Relationship::Document(target) => {
-          events.add_tag_with_attr(b"Relationship", vec![
-            // TODO: add relationship id
-            ("Type", OFFICE_DOCUMENT_SCHEMAS),
-            ("Target", target)
-          ]);
-        }
-        _ => ()
-      }
+      let (ref rel_type, target) = self.relationships[i];
+      events.add_tag_with_attr(b"Relationship", vec![
+        ("Id", IDS[i]),
+        ("Target", target),
+        ("Type", match rel_type {
+          &Relationship::Document => OFFICE_DOCUMENT_SCHEMAS,
+          &Relationship::Core => CORE_SCHEMAS,
+          &Relationship::Extended => EXTENDED_SCHEMAS,
+          // TODO: more schemas
+          _ => CORE_SCHEMAS
+        })
+      ]);
     }
 
     events
