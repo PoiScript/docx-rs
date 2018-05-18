74485e13608560c79bc6daa7407831f5099745ce
PoiScript <poiscript@gmail.com>
2018-05-18 16:12:36 +0800

    feat: add event_macro for creating xml event

---
+++ src/event_macro.rs
@@ -0,0 +1,73 @@
+macro_rules! attrs {
+  ($start:tt, $key:tt = $value:tt) => {{
+    $start.push_attribute(($key, $value));
+  }};
+
+  ($start:tt, $key:tt = $value:tt $($rest:tt)*) => {{
+    $start.push_attribute(($key, $value));
+    attrs!($start, $($rest)*);
+  }};
+}
+
+macro_rules! write_empty_event {
+  ($writer:ident, $tag:tt) => {{
+    $writer.write_event(Event::Empty(BytesStart::borrowed($tag, $tag.len())))?;
+  }};
+
+  ($writer:ident, $tag:tt [ $($attrs:tt)* ]) => {{
+    let mut start = BytesStart::borrowed($tag, $tag.len());
+    attrs!(start, $($attrs)*);
+    $writer.write_event(Event::Empty(start))?;
+  }};
+}
+
+macro_rules! write_start_event {
+  ($writer:ident, $tag:tt) => {{
+    $writer.write_event(Event::Start(BytesStart::borrowed($tag, $tag.len())))?;
+  }};
+
+  ($writer:ident, $tag:tt [ $($attrs:tt)* ]) => {{
+    let mut start = BytesStart::borrowed($tag, $tag.len());
+    attrs!(start, $($attrs)*);
+    $writer.write_event(Event::Start(start))?;
+  }};
+}
+
+macro_rules! write_text_event {
+  ($writer:ident, $tag:tt) => {{
+    $writer.write_event(Event::Text(BytesText::from_plain($tag.as_bytes())))?;
+  }};
+}
+
+macro_rules! write_end_event {
+  ($writer:ident, $tag:tt) => {{
+    $writer.write_event(Event::End(BytesEnd::borrowed($tag)))?;
+  }};
+}
+
+macro_rules! write_events {
+  ($writer:ident,) => (());
+
+  ($writer:ident, $tag:expr) => {{
+    write_text_event!($writer, $tag);
+  }};
+
+  ($writer:ident, $tag:tt [ $($attrs:tt)* ] { $($inner:tt)* } $($rest:tt)*) => {{
+    write_start_event!($writer, $tag [ $($attrs)* ]);
+    write_events!($writer, $($inner)*);
+    write_end_event!($writer, $tag);
+    write_events!($writer, $($rest)*);
+  }};
+
+  ($writer:ident, $tag:tt [ $($attrs:tt)* ] $($rest:tt)*) => {{
+    write_empty_event!($writer, $tag [ $($attrs)* ]);
+    write_events!($writer, $($rest)*);
+  }};
+
+  ($writer:ident, $tag:tt { $($inner:tt)* } $($rest:tt)*) => {{
+    write_start_event!($writer, $tag);
+    write_events!($writer, $($inner)*);
+    write_end_event!($writer, $tag);
+    write_events!($writer, $($rest)*);
+  }};
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,12 @@
+#![feature(trace_macros)]
 extern crate quick_xml;
 extern crate zip;
 
 mod body;
 mod content_type;
 mod docx;
+#[macro_use]
+mod event_macro;
 mod events_list;
 mod schema;
 mod xml;
